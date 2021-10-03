4ade4e00d01a21c25691715e6e157d2e2161da1a
mosuke5 <w.vamos603@gmail.com>
2021-10-03 16:44:10 +0900

    Update init.sh

--- db/init.sh
+++ db/init.sh
@@ -3,12 +3,12 @@ set -xe
 set -o pipefail
 
 CURRENT_DIR=$(cd $(dirname $0);pwd)
-export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
-export MYSQL_PORT=${MYSQL_PORT:-3306}
-export MYSQL_USER=${MYSQL_USER:-isucon}
-export MYSQL_DBNAME=${MYSQL_DBNAME:-isuumo}
-export MYSQL_PWD=${MYSQL_PASS:-isucon}
+export JOB_DB_HOST=${DB:-127.0.0.1}
+export JOB_DB_PORT=${DB_PORT:-3306}
+export JOB_DB_USER=${DB_USER:-isucon}
+export JOB_DB_DBNAME=${DB_DBNAME:-isuumo}
+export JOB_DB_PWD=${DB_PASS:-isucon}
 export LANG="C.UTF-8"
 cd $CURRENT_DIR
 
-cat 0_Schema.sql 1_DummyEstateData.sql 2_DummyChairData.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+cat 0_Schema.sql 1_DummyEstateData.sql 2_DummyChairData.sql | mysql --defaults-file=/dev/null -h $JOB_DB_HOST -P $JOB_DB_PORT -u $JOB_DB_USER $JOB_DB_DBNAME
