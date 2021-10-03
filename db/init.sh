b6a46a79f555377a0bfce85e5bd65d2a53abdc46
mosuke5 <w.vamos603@gmail.com>
2021-10-03 16:50:26 +0900

    Update init.sh

--- db/init.sh
+++ db/init.sh
@@ -3,7 +3,7 @@ set -xe
 set -o pipefail
 
 CURRENT_DIR=$(cd $(dirname $0);pwd)
-export JOB_DB_HOST=${DB:-127.0.0.1}
+export JOB_DB_HOST=${DB_HOST:-127.0.0.1}
 export JOB_DB_PORT=${DB_PORT:-3306}
 export JOB_DB_USER=${DB_USER:-isucon}
 export JOB_DB_DBNAME=${DB_DBNAME:-isuumo}
